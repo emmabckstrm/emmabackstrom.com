9e00de7dd34b182c7a56e6236f7c68a997adcf01
emmabckstrm <backstromemma@gmail.com>
2020-01-26 12:26:44 +0100

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+const Index = () => (
+    <div>
+      <h1 class="center">emma bäckström</h1>
+     <style jsx>{`
+     div {
+         position: relative;
+         height: 100%;
+         display: flex;
+         flex-direction: row;
+         align-items: center;
+         justify-content: center;
+         text-align: center; 
+     }
+     h1 {
+        font-family: 'courier';
+        margin: 0;
+      }
+     `}</style>
+    </div>
+  );
+  
+  export default Index;
+  
