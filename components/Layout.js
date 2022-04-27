ed030032088dee359cbd87b9d0b0928490de87fe
emmabckstrm <8473077+emmabckstrm@users.noreply.github.com>
2022-04-27 17:23:17 +0200

    add favicon

--- components/Layout.js
+++ components/Layout.js
@@ -1,26 +1,42 @@
 function Layout(props) {
-    return (
-      <>
-        {props.children}
-        <style jsx global>{`
-          html, body, #__next {
-            position: relative;
-            width: 100%;
-            height: 100%;
-            margin: 0;
-            padding: 0;
-          }
-          body {
-            font-size: 18px;
-            font-weight: 400;
-            line-height: 1.8;
-            color: #333;
-            background: #eee;
-            font-family: sans-serif;
-          }
-        `}</style>
-      </>
-    )
-  }
-  
-  export default Layout;
+  return (
+    <>
+      <link
+        rel="icon"
+        type="image/png"
+        href="/favicon-16x16.png"
+        sizes="16x16"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        href="/favicon-32x32.png"
+        sizes="32x32"
+      />
+      <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+
+      {props.children}
+      <style jsx global>{`
+        html,
+        body,
+        #__next {
+          position: relative;
+          width: 100%;
+          height: 100%;
+          margin: 0;
+          padding: 0;
+        }
+        body {
+          font-size: 18px;
+          font-weight: 400;
+          line-height: 1.8;
+          color: #333;
+          background: #eee;
+          font-family: sans-serif;
+        }
+      `}</style>
+    </>
+  );
+}
+
+export default Layout;
