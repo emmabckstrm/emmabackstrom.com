4409e0220d41ac65aabe5eaa09003890e88c6cda
emmabckstrm <8473077+emmabckstrm@users.noreply.github.com>
2022-04-27 17:32:31 +0200

    add meta tags

--- components/Layout.js
+++ components/Layout.js
@@ -1,19 +1,41 @@
+import Head from "next/head";
+
+const URL = "https://emmabackstrom.com/";
+const TITLE = "Emma Bäckström";
+const DESCRIPTION = "Frontend/fullstack Engineer";
+
 function Layout(props) {
   return (
     <>
-      <link
-        rel="icon"
-        type="image/png"
-        href="/favicon-16x16.png"
-        sizes="16x16"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        href="/favicon-32x32.png"
-        sizes="32x32"
-      />
-      <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      <Head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-16x16.png"
+          sizes="16x16"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-32x32.png"
+          sizes="32x32"
+        />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <title>Emma Bäckström</title>
+        <meta name="title" content={TITLE} />
+        <meta name="description" content={DESCRIPTION} />
+
+        {/* Open Graph */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={URL} />
+        <meta property="og:title" content={TITLE} />
+        <meta property="og:description" content={DESCRIPTION} />
+
+        {/* Twitter */}
+        <meta property="twitter:url" content={URL} />
+        <meta property="twitter:title" content={TITLE} />
+        <meta property="twitter:description" content={DESCRIPTION} />
+      </Head>
 
       {props.children}
       <style jsx global>{`
