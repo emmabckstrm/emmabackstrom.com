ce58bdb575857810cfc8a54423e0e0f2b675eda4
Emma Bäckström <8473077+emmabckstrm@users.noreply.github.com>
2023-06-05 13:15:24 +0200

    chore: complete storybook setup for usage

--- app/sweeper/components/Square.tsx
+++ app/sweeper/components/Square.tsx
@@ -11,8 +11,22 @@ export const Square = ({
   isBomb: boolean;
   isOpen: boolean;
 }) => {
+  const baseClasses = "font-mono p-2 border rounded w-10 h-10";
+  const unopenedClasses = "bg-purple-400 border-purple-700";
+  const openClasses = "bg-purple-100 border-purple-300";
+  const bombClasses = "bg-amber-500 border-amber-700";
+
+  let style;
+  if (!isOpen) {
+    style = unopenedClasses;
+  } else if (isBomb) {
+    style = bombClasses;
+  } else {
+    style = openClasses;
+  }
+
   return (
-    <button className="font-mono bg-blue-700 text-white p-2 border rounded w-10 h-10">
+    <button className={`${baseClasses} ${style}`}>
       {(isBomb && "X") || adjacentBombs}
     </button>
   );
